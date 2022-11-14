193fe6918f88a6a6b47a4a2be1604edd1ce984a7
Ilona Svidritskaia <isvidritskaia@gmail.com>
2022-11-13 19:58:31 -0500

    Задание 9. Незаконченное

--- js/main.js
+++ js/main.js
@@ -1,21 +1,61 @@
-function getRandom (min, max) {
-  if (min < 0 || max < 0) {
+// id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
+// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+// description, строка — описание фотографии. Описание придумайте самостоятельно.
+// likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+// comments, число — количество комментариев, оставленных другими пользователями к этой фотографии. Случайное число от 0 до 200.
+
+const DESCRIPTION = [
+  'Красиво',
+  'Прекрасно',
+  'Отдых',
+  'Работа',
+  'Весело',
+  'Грустно',
+  'Радостно',
+  'Печально',
+  'Тихо',
+  'Спокойно',
+  'Неспокойно',
+  'Наслаждение',
+  'Ветрено',
+  'Холодно',
+  'Тепло',
+  'Жарко',
+  'Кайфуем',
+  'Расслабляемся',
+  'Не напрягаемся',
+  'Свобода',
+  'Солнечно',
+  'Захватывающе',
+  'Непередаваемые ощущения',
+  'Никогда такого не видел',
+  'Отличный вид'
+];
+
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
     return NaN;
   }
-  if (max < min) {
-    return NaN;
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
 }
 
-function checkLength (text, maxLength) {
-  if (text.length <= maxLength) {
-    return true;
-  }
-  return false;
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
 
-getRandom(10, 20);
-checkLength('ldghjf', 5);
+const createPhotoInfo = () => ({
+  id: getRandomPositiveInteger(1, 25),
+  url: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+  description: DESCRIPTION[getRandomPositiveInteger(1, DESCRIPTION.length)],
+  likes: getRandomPositiveInteger(15, 200),
+  comments: getRandomPositiveInteger(0, 200)
+});
+
+const similarPhotoInfo = Array.from({length: 25}, createPhotoInfo);
+
+console.log(similarPhotoInfo);
+
